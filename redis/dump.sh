ae6c73bf70f5fad55cebbf13a9433a50e820ba30
bobwei <bob61016@gmail.com>
2016-04-10 01:01:48 +0800

    init

---
+++ redis/dump.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+REDIS_CONTAINER_NAME=${PWD##*/}_redis_1
+BACKUP_PATH=$(pwd)/redis/data
+
+docker run --rm --volumes-from $REDIS_CONTAINER_NAME -v $BACKUP_PATH:/backup \
+  busybox tar cvf /backup/redis.tar /data
